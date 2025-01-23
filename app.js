16e488799f494d406f1b7cb66995efd7588eee79
Yufei-Chang <yufei.chang@hotmail.com>
2025-01-23 18:52:43 +0100

    feat: implementati middleware

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express()
 const port = 3001
 // Pijio le rotte
 const router = require("./routes/movieRoute");
+const movieErr = require("./middleware/movieErr");
 
 app.use(express.static("public"));
 app.use(express.json());
@@ -16,6 +17,8 @@ app.get('/', (req, res) => {
 app.use((req, res, next) => {
     res.status(404).send("Dispersi nel mare delle lacrime");
   });
+
+app.use(movieErr);
 
 app.listen(port, () => {
   console.log(`Pronto? Pronto?! Me stai a sentì? ${port}`)
---
+++ middleware/movieErr.js
@@ -0,0 +1,8 @@
+function movieErr(err, req, res, next) {
+    res.statusCode = 500;
+    res.json({
+      error: true,
+      message: "Errore interno del server"
+    });}
+
+module.exports = movieErr;
