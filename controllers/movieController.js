80acf8faafb7c496df2a284bd4a3fc7b3c0dbeff
Yufei-Chang <yufei.chang@hotmail.com>
2025-01-24 10:39:47 +0100

    feat: implementato funzione search per filter hehehehe

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -1,35 +1,47 @@
 const connection = require("../db")
 
 // Definisco gli endpoint dei callback, ovvero i codici che vengono eseguiti in un url specifico
-function index(req, res) {
-    const sql = "SELECT * FROM movies";
+function index(req, res, next) {
+  // Ricerca per FILTRO - Prelevo query string params
+  const filters = req.query;
+  console.log(filters);
 
-    connection.query(sql, (err, movies) => {
-        if (err) {
-          return next(new Error(err.message));
-        }
-    
-        return res.status(200).json({
-          status: "success",
-          data: movies,
-        });
-      });
+  let sql = "SELECT * FROM `movies`";
+  const params = [];
+
+  if (filters.search) {
+    sql += `
+  WHERE title LIKE ?;
+  `;
+    params.push(`%${filters.search}%`);
+  };
+
+  connection.query(sql, params, (err, movies) => {
+    if (err) {
+      return next(new Error(err.message));
     }
 
-function show(req, res) {
-    const urlId = req.params.id;
-    const sql = "SELECT * FROM movies WHERE id = ?";
+    return res.status(200).json({
+      status: "success",
+      data: movies,
+    });
+  });
+}
 
-    connection.query(sql, [urlId], (err, movies) => {
-        if (err) {
-          return next(new Error(err.message));
-        }
-    
-        return res.status(200).json({
-          status: "success",
-          data: movies,
-        });
-      });
+function show(req, res) {
+  const urlId = req.params.id;
+  const sql = "SELECT * FROM movies WHERE id = ?";
+
+  connection.query(sql, [urlId], (err, movies) => {
+    if (err) {
+      return next(new Error(err.message));
+    }
+
+    return res.status(200).json({
+      status: "success",
+      data: movies,
+    });
+  });
 }
 
 module.exports = { index, show }
